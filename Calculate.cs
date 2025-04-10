69a0f50aed9cee72799cfd5bf5d717fb883e94d5
jay-tell3 <jaybroww@gmail.com>
2025-04-10 15:07:53 -0500

    Create Calculate.cs

---
+++ Calculate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatucMembers_brown_Jaylen
+{
+    static internal class Calculate
+    {
+
+        public static float result = 0.0f;
+
+        static Calculate()
+        {
+            result = 0.0f;        
+        }
+
+        public static int Add(int x, int y)
+        { 
+           return x + y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Mult(float x, float y)
+        {
+            return x * y;
+        }
+
+        public static float Div(float x, float y)
+        {
+            return x / y;
+        }
+
+    }
+}
