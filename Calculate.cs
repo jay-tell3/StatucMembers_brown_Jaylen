cc8e0c099272d02b0beb096adb7f9c339a92f0f0
jay-tell3 <jaybroww@gmail.com>
2025-04-11 14:25:30 -0500

    done

--- Calculate.cs
+++ Calculate.cs
@@ -5,52 +5,53 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace StatucMembers_brown_Jaylen
-{
+{   //makes the Calculate class
     static internal class Calculate
     {
-
+        //the result of the answer
         public static float result = 0.0f;
-
+        //inislizes the result 
         static Calculate()
         {
             result = 0.0f;        
         }
-
+        //methods\/
+        //Adds two ints and retruns the answer
         public static int Add(int x, int y)
         { 
            return x + y;
         }
-
+        //subtracts two ints and retruns the answer
         public static int Sub(int x, int y)
         {
             return x - y;
         }
-
+        //multiplis two ints and retruns the answer
         public static int Mult(int x, int y)
         {
             return x * y;
         }
-
+        //divides two ints and retruns the answer
         public static int Div(int x, int y)
         {
             return x / y;
         }
-
+        //Adds two floats and retruns the answer
         public static float Add(float x, float y)
         {
             return x + y;
         }
-
+        //subtracts two floats and retruns the answer
         public static float Sub(float x, float y)
         {
             return x - y;
         }
-
+        //multiplis two floats and retruns the answer
         public static float Mult(float x, float y)
         {
             return x * y;
         }
-
+        //divids two floats and retruns the answer
         public static float Div(float x, float y)
         {
             return x / y;
--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //answers of ints
+            Console.WriteLine(Calculate.Add(15, 88));
+            Console.WriteLine(Calculate.Sub(15, 88));
+            Console.WriteLine(Calculate.Mult(15, 88));
+            Console.WriteLine(Calculate.Div(88, 15));
+            //answers of floats
+            Console.WriteLine(Calculate.Add(15.23f, 88.23f));
+            Console.WriteLine(Calculate.Sub(15.67f, 88.89f));
+            Console.WriteLine(Calculate.Mult(15.76f, 88.39f));
+            Console.WriteLine(Calculate.Div(88.68f, 15.12f));
+
+
         }
     }
 }
